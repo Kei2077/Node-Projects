5c56aa12e3e2f8992f228185da78ae98e836aaaa
Matheus Luiz M. Silva <131410397+Kei2077@users.noreply.github.com>
2025-09-04 20:32:21 -0300

    Add initial Hello World, math, and name scripts
    Introduces three new files: app.js for Hello World output, math.js for a simple addition function and console log, and nome.js for concatenating and displaying a name and surname.

---
+++ Hello/math.js
@@ -0,0 +1,7 @@
+var num = 27
+var num1 = 50
+
+function soma(a,b){
+    return a + b;
+}
+console.log(soma(num,num1))
---
+++ Hello/nome.js
@@ -0,0 +1,3 @@
+var nome = "João"
+var sobrenome = "Ninguêm"
+console.log(nome + " " + sobrenome); //precisa usar " " para usar espaço

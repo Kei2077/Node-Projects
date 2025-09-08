3871890e96aa53ee599e98ebba5408bc8430e5c6
Matheus Luiz M. Silva <131410397+Kei2077@users.noreply.github.com>
2025-09-08 19:35:52 -0300

    Add Sequelize connection and models for MySQL
    Introduces connect-node.js to establish a Sequelize connection to a MySQL database and models.js to define 'postagens' and 'usuarios' models. Includes example model creation and sync code (commented out) for initial setup.

---
+++ SQL/connect-node.js
@@ -0,0 +1,32 @@
+
+const Sequelize = require('sequelize')
+
+
+const sequelize = new Sequelize('chach', 'root', 'REDACTED', {
+
+
+    host: "localhost",
+
+
+    dialect: 'mysql'
+
+
+})
+
+
+
+
+
+sequelize.authenticate().then(function(){
+
+
+    console.log("Conectado!")
+
+
+}).catch(function(erro){
+
+
+    console.log("falhou: "+erro)
+
+
+})
---
+++ SQL/models.js
@@ -0,0 +1,47 @@
+
+const Sequelize = require('sequelize')
+
+
+const sequelize = new Sequelize('chach', 'root', '23010777', {
+
+
+    host: "localhost",
+
+
+    dialect: 'mysql'
+
+
+})
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+})
+/*Postagem.create({
+    titulo: "A vila do chach",
+    conteudo: "Uma xicará de café?"
+}) */
+const user = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+//user.sync({force: true})
+/* user.create({
+    nome: "Victor",
+    sobrenome: "Black",
+    idade: 35,
+    email: "victorino@email.com"
+}) */
